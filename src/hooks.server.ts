1935e3e14f68fbaee58d85b2e5fde56764906867
RedCrafter07 <gh@r07.dev>
2026-01-08 15:01:09 +0100

    fix(hooks): Lazy load auth module in handle function to prevent interference with initialization

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
-import { auth } from '$lib/server/auth';
-import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { building } from '$app/environment';
 import { userInit } from '$lib/server/userInit';
+import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export async function init() {
 	await userInit();
 }
 
 export async function handle({ event, resolve }) {
+	const { auth } = await import('$lib/server/auth');
 	return svelteKitHandler({ event, resolve, auth, building });
 }
