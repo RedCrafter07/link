988c37c22aa2a4a1c48e94bffb775dd1d1f5ed46
RedCrafter07 <gh@r07.dev>
2026-01-10 14:48:37 +0100

    refactor(redirects): Rename import from redirects to redirectTable for clarity

--- src/routes/[...catchall]/+page.server.ts
+++ src/routes/[...catchall]/+page.server.ts
@@ -1,22 +1,22 @@
 import { db } from '$lib/server/db/index.js';
-import { redirect as redirects } from '$lib/server/db/schema/redirect.js';
+import { redirect as redirectTable } from '$lib/server/db/schema/redirect.js';
 import { error, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 
 export const load = async ({ url }) => {
 	const path = url.pathname;
 
-	const query = await db.select().from(redirects).where(eq(redirects.from, path));
+	const query = await db.select().from(redirectTable).where(eq(redirectTable.from, path));
 
 	if (query.length > 1) return error(501, 'Multiple redirect found.');
 	else if (query.length == 1) {
 		await db
-			.update(redirects)
+			.update(redirectTable)
 			.set({
 				...query[0],
 				accessCount: query[0].accessCount + 1
 			})
-			.where(eq(redirects.from, query[0].from));
+			.where(eq(redirectTable.from, query[0].from));
 
 		return redirect(307, query[0].to);
 	}
