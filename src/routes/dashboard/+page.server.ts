fcfa98e8fcbe4511776c78dec21b3ab8952bb9f3
RedCrafter07 <gh@r07.dev>
2026-01-13 20:53:38 +0100

    feat(dashboard): Enhance dashboard page with redirect statistics and improved layout

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,3 +1,15 @@
+import { db } from '$lib/server/db';
+
 export const load = async ({ locals }) => {
-	return { user: locals.user };
+	const redirects = await db.query.redirect.findMany({
+		with: {
+			logs: true
+		}
+	});
+	const redirectCount = redirects.length;
+	const redirectVisitCount = redirects.reduce((prev, c) => {
+		return prev + c.logs.length;
+	}, 0);
+
+	return { user: locals.user, redirectCount, redirectVisitCount };
 };
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	import IconRouteSquare2 from '~icons/tabler/route-square-2';
 	const { data } = $props();
 </script>
 
-Hello {data.user.name}!
+<div class="flex flex-col gap-8">
+	<h1 class="text-3xl font-semibold tracking-wide">
+		Welcome to Link, {data.user.name}!
+	</h1>
+
+	<div class="grid w-full grid-cols-1 lg:grid-cols-2">
+		<button
+			class="relative top-0 flex flex-row items-center gap-4 rounded-xl border border-transparent bg-base-2 p-4 text-left transition-all duration-150 hover:border-overlay/50 active:top-1 active:scale-[99%] active:drop-shadow-lg"
+			onclick={() => {
+				goto(resolve('/dashboard/redirects'));
+			}}
+		>
+			<div class="text-2xl">
+				<IconRouteSquare2 />
+			</div>
+			<div>
+				<h3 class="text-xl">Manage Redirects</h3>
+				<p>
+					{data.redirectCount.toString()}
+					{data.redirectCount === 1 ? 'redirect' : 'redirects'} found with {data.redirectVisitCount.toString()}
+					{data.redirectVisitCount === 1 ? 'visit' : 'visits'} total
+				</p>
+			</div>
+		</button>
+	</div>
+</div>
